d0a17a93f09691d88d5691c3e80f1902c37f201a
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-03-05 15:58:34 +0530

    added html to page-contact.php

--- page-about.php
+++ page-about.php
@@ -98,7 +98,7 @@ Template Name: About Page
                 </p>
 
                 <div class="footer-social">
-                   <a href="#"  aria-label="Facebook" target = "_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></a>
+         <a href="#"  aria-label="Facebook" target = "_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></a>
           <a href="https://www.instagram.com/" aria-label="Instagram" target = "_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" alt=""></a>
           <a href="#" aria-label="Twitter" target = "_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/1707222563twitter-logo-png.png" alt=""></a>
                 </div>
