9d3f07cfcfb5dab9862df88ad3671d2eeeb9a523
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-03-06 16:45:57 +0530

    update the theme

--- footer.php
+++ footer.php
@@ -1,11 +1,4 @@
 <footer>
 <?php wp_footer(); ?>
- <p class="footer-email">
-       Email:
-       <?php
-       $email = "sharmapiyush1342@gmail.com";
-       echo '<a href="mailto:'.$email.'?subject=Support Request">'.$email.'</a>';
-        ?>
-</p>
 </body>
 </html>
