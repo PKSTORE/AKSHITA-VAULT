6f2f5500bde268831f5b458368593a81e72aff9a
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-03-06 15:52:21 +0530

    added the html file to function.php

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
-function mycustomtheme_enqueue_styles() {
+function mycustomtheme_enqueue_styles()
+{
     wp_enqueue_style(
         'mycustomtheme-style',
         get_stylesheet_uri(),
@@ -9,233 +10,242 @@ function mycustomtheme_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'mycustomtheme_enqueue_styles');
 /* Enquiry Form */
-function rr_create_enquiry_post_type(){
+function rr_create_enquiry_post_type()
+{
 
-register_post_type('rr_enquiry', array(
+    register_post_type('rr_enquiry', array(
 
-'labels' => array(
-'name' => 'Enquiries',
-'singular_name' => 'Enquiry'
-),
+        'labels' => array(
+            'name' => 'Enquiries',
+            'singular_name' => 'Enquiry'
+        ),
 
-'public' => false,
-'show_ui' => true,
-'menu_icon' => 'dashicons-email',
-'supports' => array('title','editor')
+        'public' => false,
+        'show_ui' => true,
+        'menu_icon' => 'dashicons-email',
+        'supports' => array('title', 'editor')
 
-));
-
-}
-
-add_action('init','rr_create_enquiry_post_type');
-function rr_load_ajax_script(){
-
-wp_enqueue_script('rr-ajax', get_template_directory_uri().'/ajax.js', array(), null, true);
-
-wp_localize_script('rr-ajax','rr_ajax',array(
-'ajax_url' => admin_url('admin-ajax.php')
-));
+    ));
 
 }
 
-add_action('wp_enqueue_scripts','rr_load_ajax_script');
-function rr_save_enquiry(){
+add_action('init', 'rr_create_enquiry_post_type');
+function rr_load_ajax_script()
+{
 
-$name = sanitize_text_field($_POST['client_name']);
-$phone = sanitize_text_field($_POST['client_phone']);
+    wp_enqueue_script('rr-ajax', get_template_directory_uri() . '/ajax.js', array(), null, true);
 
-if(empty($name) || empty($phone)){
-wp_send_json_error();
-}
-
-/* Save to WP */
-
-$post_id = wp_insert_post(array(
-'post_title' => $name,
-'post_content' => 'Phone: '.$phone,
-'post_type' => 'rr_enquiry',
-'post_status' => 'publish'
-));
-
-/* Email Notification */
-
-$admin_email = get_option('sharmapiyush1342@gmail.com');
-
-wp_mail(
-$admin_email,
-"New Enquiry Received",
-"Name: $name \nPhone: $phone"
-);
-
-wp_send_json_success();
+    wp_localize_script('rr-ajax', 'rr_ajax', array(
+        'ajax_url' => admin_url('admin-ajax.php')
+    ));
 
 }
 
-add_action('wp_ajax_save_enquiry','rr_save_enquiry');
-add_action('wp_ajax_nopriv_save_enquiry','rr_save_enquiry');
-function rr_enquiry_columns($columns){
+add_action('wp_enqueue_scripts', 'rr_load_ajax_script');
+function rr_save_enquiry()
+{
 
-$columns['phone'] = 'Phone';
+    $name = sanitize_text_field($_POST['client_name']);
+    $phone = sanitize_text_field($_POST['client_phone']);
 
-return $columns;
+    if (empty($name) || empty($phone)) {
+        wp_send_json_error();
+    }
+
+    /* Save to WP */
+
+    $post_id = wp_insert_post(array(
+        'post_title' => $name,
+        'post_content' => 'Phone: ' . $phone,
+        'post_type' => 'rr_enquiry',
+        'post_status' => 'publish'
+    ));
+
+    /* Email Notification */
+
+    $admin_email = get_option('sharmapiyush1342@gmail.com');
+
+    wp_mail(
+        $admin_email,
+        "New Enquiry Received",
+        "Name: $name \nPhone: $phone"
+    );
+
+    wp_send_json_success();
 
 }
 
-add_filter('manage_rr_enquiry_posts_columns','rr_enquiry_columns');
+add_action('wp_ajax_save_enquiry', 'rr_save_enquiry');
+add_action('wp_ajax_nopriv_save_enquiry', 'rr_save_enquiry');
+function rr_enquiry_columns($columns)
+{
 
+    $columns['phone'] = 'Phone';
 
-function rr_enquiry_column_data($column,$post_id){
-
-if($column == 'phone'){
-
-$content = get_post_field('post_content',$post_id);
-echo $content;
-
-}
+    return $columns;
 
 }
 
-add_action('manage_rr_enquiry_posts_custom_column','rr_enquiry_column_data',10,2);
+add_filter('manage_rr_enquiry_posts_columns', 'rr_enquiry_columns');
+
+
+function rr_enquiry_column_data($column, $post_id)
+{
+
+    if ($column == 'phone') {
+
+        $content = get_post_field('post_content', $post_id);
+        echo $content;
+
+    }
+
+}
+
+add_action('manage_rr_enquiry_posts_custom_column', 'rr_enquiry_column_data', 10, 2);
 /* CONTACT MESSAGES POST TYPE */
 
-function rr_create_contact_post_type(){
+function rr_create_contact_post_type()
+{
 
-register_post_type('rr_contact', array(
+    register_post_type('rr_contact', array(
 
-'labels' => array(
-'name' => 'Contact Messages',
-'singular_name' => 'Contact Message'
-),
+        'labels' => array(
+            'name' => 'Contact Messages',
+            'singular_name' => 'Contact Message'
+        ),
 
-'public' => false,
-'show_ui' => true,
-'menu_icon' => 'dashicons-email',
-'supports' => array('title','editor')
+        'public' => false,
+        'show_ui' => true,
+        'menu_icon' => 'dashicons-email',
+        'supports' => array('title', 'editor')
 
-));
+    ));
 
 }
 
-add_action('init','rr_create_contact_post_type');
+add_action('init', 'rr_create_contact_post_type');
 
 
 
 /* SAVE CONTACT FORM DATA */
 
-function rr_save_contact_data(){
+function rr_save_contact_data()
+{
 
-if(isset($_POST['submit_contact'])){
+    if (isset($_POST['submit_contact'])) {
 
-$name = sanitize_text_field($_POST['contact_name']);
-$phone = sanitize_text_field($_POST['contact_phone']);
-$email = sanitize_email($_POST['contact_email']);
-$message = sanitize_textarea_field($_POST['contact_message']);
-
-
-/* Save in WordPress Dashboard */
-
-wp_insert_post(array(
-
-'post_title' => $name,
-
-'post_content' =>
-"Phone: ".$phone."\n".
-"Email: ".$email."\n\n".
-"Message:\n".$message,
-
-'post_type' => 'rr_contact',
-
-'post_status' => 'publish'
-
-));
+        $name = sanitize_text_field($_POST['contact_name']);
+        $phone = sanitize_text_field($_POST['contact_phone']);
+        $email = sanitize_email($_POST['contact_email']);
+        $message = sanitize_textarea_field($_POST['contact_message']);
 
 
-/* Send Email Notification */
+        /* Save in WordPress Dashboard */
 
-$admin_email = get_option('admin_email');
+        wp_insert_post(array(
 
-$subject = "New Contact Message - RR Computers";
+            'post_title' => $name,
 
-$body = "You received a new contact request.\n\n";
-$body .= "Name: ".$name."\n";
-$body .= "Phone: ".$phone."\n";
-$body .= "Email: ".$email."\n\n";
-$body .= "Message:\n".$message."\n";
+            'post_content' =>
+                "Phone: " . $phone . "\n" .
+                "Email: " . $email . "\n\n" .
+                "Message:\n" . $message,
 
-wp_mail($admin_email,$subject,$body);
+            'post_type' => 'rr_contact',
+
+            'post_status' => 'publish'
+
+        ));
+
+
+        /* Send Email Notification */
+
+        $admin_email = get_option('admin_email');
+
+        $subject = "New Contact Message - RR Computers";
+
+        $body = "You received a new contact request.\n\n";
+        $body .= "Name: " . $name . "\n";
+        $body .= "Phone: " . $phone . "\n";
+        $body .= "Email: " . $email . "\n\n";
+        $body .= "Message:\n" . $message . "\n";
+
+        wp_mail($admin_email, $subject, $body);
+
+    }
 
 }
 
-}
-
-add_action('init','rr_save_contact_data');
+add_action('init', 'rr_save_contact_data');
 /* CREATE COMPLAINT POST TYPE */
 
-function rr_create_complaint_post_type(){
+function rr_create_complaint_post_type()
+{
 
-register_post_type('rr_complaint', array(
+    register_post_type('rr_complaint', array(
 
-'labels' => array(
-'name' => 'Complaints',
-'singular_name' => 'Complaint'
-),
+        'labels' => array(
+            'name' => 'Complaints',
+            'singular_name' => 'Complaint'
+        ),
 
-'public' => false,
-'show_ui' => true,
-'menu_icon' => 'dashicons-warning',
-'supports' => array('title','editor')
+        'public' => false,
+        'show_ui' => true,
+        'menu_icon' => 'dashicons-warning',
+        'supports' => array('title', 'editor')
 
-));
+    ));
 
 }
 
-add_action('init','rr_create_complaint_post_type');
+add_action('init', 'rr_create_complaint_post_type');
 
 
 
 /* SAVE COMPLAINT DATA */
 
-function rr_save_complaint_data(){
+function rr_save_complaint_data()
+{
 
-if(isset($_POST['submit_complaint'])){
+    if (isset($_POST['submit_complaint'])) {
 
-$name = sanitize_text_field($_POST['complaint_name']);
-$phone = sanitize_text_field($_POST['complaint_phone']);
-$message = sanitize_textarea_field($_POST['complaint_message']);
-
-
-/* Save complaint in dashboard */
-
-wp_insert_post(array(
-
-'post_title' => $name,
-
-'post_content' =>
-"Phone: ".$phone."\n\n".
-"Complaint:\n".$message,
-
-'post_type' => 'rr_complaint',
-
-'post_status' => 'publish'
-
-));
+        $name = sanitize_text_field($_POST['complaint_name']);
+        $phone = sanitize_text_field($_POST['complaint_phone']);
+        $message = sanitize_textarea_field($_POST['complaint_message']);
 
 
-/* Send email notification */
+        /* Save complaint in dashboard */
 
-$admin_email = get_option('admin_email');
+        wp_insert_post(array(
 
-$subject = "New Complaint Submitted - RR Computers";
+            'post_title' => $name,
 
-$body = "A new complaint has been submitted.\n\n";
-$body .= "Name: ".$name."\n";
-$body .= "Phone: ".$phone."\n\n";
-$body .= "Complaint:\n".$message."\n";
+            'post_content' =>
+                "Phone: " . $phone . "\n\n" .
+                "Complaint:\n" . $message,
 
-wp_mail($admin_email,$subject,$body);
+            'post_type' => 'rr_complaint',
+
+            'post_status' => 'publish'
+
+        ));
+
+
+        /* Send email notification */
+
+        $admin_email = get_option('admin_email');
+
+        $subject = "New Complaint Submitted - RR Computers";
+
+        $body = "A new complaint has been submitted.\n\n";
+        $body .= "Name: " . $name . "\n";
+        $body .= "Phone: " . $phone . "\n\n";
+        $body .= "Complaint:\n" . $message . "\n";
+
+        wp_mail($admin_email, $subject, $body);
+
+    }
 
 }
 
-}
-
-add_action('init','rr_save_complaint_data');
+add_action('init', 'rr_save_complaint_data');
--- page-contact.php
+++ page-contact.php
@@ -1,171 +1,177 @@
 <?php get_header(); ?>
- <header class="navbar">
-    <div class="nav-container">
+<header class="navbar">
+  <div class="nav-container">
 
-      <!-- Logo -->
-      <div class="logo">
+    <!-- Logo -->
+    <div class="logo">
       <a href="<?php echo site_url('/'); ?>">
-  <img src="<?php echo get_template_directory_uri(); ?>/img/RRC-Long-logo.png">
-        </a>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/RRC-Long-logo.png">
+      </a>
 
-      </div>
+    </div>
 
-      <!-- Hamburger -->
-      <div class="hamburger" onclick="toggleMenu()">
-        ☰
-      </div>
+    <!-- Hamburger -->
+    <div class="hamburger" onclick="toggleMenu()">
+      ☰
+    </div>
 
-      <!-- Menu -->
-      <nav class="menu" id="navMenu">
-    <a href="<?php echo site_url('/'); ?>">Home</a>
-    <a href="<?php echo site_url('/about'); ?>">About</a>
-    <a href="<?php echo site_url('/services'); ?>">Services</a>
-    <a href="<?php echo site_url('/contact'); ?>">Contact</a>
-    <a href="<?php echo site_url('/rental'); ?>">Rental</a>
-    <!-- <a href="#ticket" class="mobile-btn">Complaint Ticket</a> -->
-</nav>
-        
-    
+    <!-- Menu -->
+    <nav class="menu" id="navMenu">
+      <a href="<?php echo site_url('/'); ?>">Home</a>
+      <a href="<?php echo site_url('/about'); ?>">About</a>
+      <a href="<?php echo site_url('/services'); ?>">Services</a>
+      <a href="<?php echo site_url('/contact'); ?>">Contact</a>
+      <a href="<?php echo site_url('/rental'); ?>">Rental</a>
+      <!-- <a href="#ticket" class="mobile-btn">Complaint Ticket</a> -->
+    </nav>
 
-      <!-- Desktop Button -->
-      <!-- <div class="nav-btn">
+
+
+    <!-- Desktop Button -->
+    <!-- <div class="nav-btn">
         <a href="#ticket">Complaint Ticket</a>
       </div> -->
 
-    </div>
-  </header>
-  <!-- Contact1-->
-  <section class="contact-1">
-    <div class="contact-1-container">
+  </div>
+</header>
+<!-- Contact1-->
+<section class="contact-1">
+  <div class="contact-1-container">
 
-      <!-- Left Content -->
-      <div class="contact-1-info">
-        <h2>Have Any<br>Questions?</h2>
+    <!-- Left Content -->
+    <div class="contact-1-info">
+      <h2>Have Any<br>Questions?</h2>
 
-        <p class="contact-1-desc">
-          If you have any query or suggestion or complaint, please fill the
-          form and submit, we will get back to you.
-        </p>
+      <p class="contact-1-desc">
+        If you have any query or suggestion or complaint, please fill the
+        form and submit, we will get back to you.
+      </p>
 
-        <ul class="contact-1-details">
-          <li>
-            <span>📞</span>
-            <a href="tel:+919899437756">+91 98994 37756</a>,
-            <a href="tel:+919354933007">+91 93549 33007</a>
-          </li>
+      <ul class="contact-1-details">
+        <li>
+          <span>📞</span>
+          <a href="tel:+919899437756">+91 98994 37756</a>,
+          <a href="tel:+919354933007">+91 93549 33007</a>
+        </li>
 
-          <li>
-            <span>✉️</span>
-            <a href="mailto:info@rrcomputers.net">
-              info@rrcomputers.net
-            </a>
-          </li>
+        <li>
+          <span>✉️</span>
+          <a href="mailto:info@rrcomputers.net">
+            info@rrcomputers.net
+          </a>
+        </li>
 
-          <li>
-            <span>📍</span>
-            G-9/85 RATIYA MARG SANGMA VIHAR <br>
-            New Delhi
-          </li>
+        <li>
+          <span>📍</span>
+          G-9/85 RATIYA MARG SANGMA VIHAR <br>
+          New Delhi
+        </li>
 
-          <li>
-            <span>⏰</span>
-            Monday – Friday: 08:00am – 07:00pm<br>
-            Saturday – Sunday: 10:00am – 05:00pm
-          </li>
-        </ul>
+        <li>
+          <span>⏰</span>
+          Monday – Friday: 08:00am – 07:00pm<br>
+          Saturday – Sunday: 10:00am – 05:00pm
+        </li>
+      </ul>
 
-        <div class="contact-1-social">
-         <a href="#"  aria-label="Facebook" target = "_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></a>
-          <a href="https://www.instagram.com/" aria-label="Instagram" target = "_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" alt=""></a>
-          <a href="#" aria-label="Twitter" target = "_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/1707222563twitter-logo-png.png" alt=""></a>
-         
-        </div>
+      <div class="contact-1-social">
+        <a href="#" aria-label="Facebook" target="_blank"> <img
+            src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></a>
+        <a href="https://www.instagram.com/" aria-label="Instagram" target="_blank"><img
+            src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" alt=""></a>
+        <a href="#" aria-label="Twitter" target="_blank"><img
+            src="<?php echo get_template_directory_uri(); ?>/img/1707222563twitter-logo-png.png" alt=""></a>
+
       </div>
+    </div>
 
-      <!-- Right Form -->
-      <div class="contact-1-form">
-        <form method= "post">
-          <div class="form-group">
-         <input type="text" name="contact_name" placeholder="Your name" required>
-          </div>
+    <!-- Right Form -->
+    <div class="contact-1-form">
+      <form method="post">
+        <div class="form-group">
+          <input type="text" name="contact_name" placeholder="Your name" required>
+        </div>
 
-          <div class="form-group">
-            <input type="tel" name="contact_phone" placeholder="Your phone" required>
-          </div>
+        <div class="form-group">
+          <input type="tel" name="contact_phone" placeholder="Your phone" required>
+        </div>
 
-          <div class="form-group">
+        <div class="form-group">
           <input type="email" name="contact_email" placeholder="Your mail" required>
-          </div>
-
-          <div class="form-group">
-           <textarea name="contact_message" placeholder="Your message"></textarea>
-          </div>
-
-          <button type="submit">Submit Request</button>
-        </form>
-      </div>
-
-    </div>
-  </section>
-  <!-- footer start-->
-  <footer class="site-footer">
-    <div class="footer-container">
-
-      <!-- Column 1: Address & Contact -->
-      <div class="footer-col">
-        <p>
-          G-9/ 85 RATIYA MARG SANGAM VIHAR <br>
-          NEW DELHI
-        </p>
-
-        <p class="footer-email">
-          Email:
-          <a href="mailto:info@rrcomputers.net">info@rrcomputers.net</a>
-        </p>
-
-        <div class="footer-social">
-         <a href="#"  aria-label="Facebook" target = "_blank"> <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></a>
-          <a href="https://www.instagram.com/" aria-label="Instagram" target = "_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" alt=""></a>
-          <a href="#" aria-label="Twitter" target = "_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/1707222563twitter-logo-png.png" alt=""></a>
-         
         </div>
-      </div>
 
-      <!-- Column 2: Pages -->
-      <div class="footer-col">
-        <h4>Qucik Links</h4>
-        <ul>
-                    <li><a href="<?php echo site_url('/'); ?>">Home</a></li>
-                    <li><a href="<?php echo site_url('/about'); ?>">About</a></li>
-                    <li><a href="<?php echo site_url('/services'); ?>">Services</a></li>
-                    <li><a href="<?php echo site_url('/contact'); ?>">Contact</a></li>
-                    <li><a href="<?php echo site_url('/rental'); ?>">Rental</a></li>
-                </ul>
-      </div>
+        <div class="form-group">
+          <textarea name="contact_message" placeholder="Your message"></textarea>
+        </div>
 
-      <!-- Column 3: Services -->
-      <div class="footer-col">
-        <h4>Services</h4>
-           <ul>
-          <li><a href="<?php echo site_url('/services'); ?>">Laptop Repair / Computer Repair</a></li>
-          <li><a href="<?php echo site_url('/services'); ?>">Cleaning Laptops / Replacing Thermal Paste</a></li>
-          <li><a href="<?php echo site_url('/services'); ?>">Data Recovery</a></li>
-          <li><a href="<?php echo site_url('/services'); ?>">OS Installation</a></li>
-          <li><a href="<?php echo site_url('/services'); ?>">Assembling a Personal Computer at Home</a></li>
-        </ul>
-      </div>
-
+        <button type="submit">Submit Request</button>
+      </form>
     </div>
 
-    <!-- Bottom -->
-    <div class="footer-bottom">
-      © 2026 RR COMPUTERS.
+  </div>
+</section>
+<!-- footer start-->
+<footer class="site-footer">
+  <div class="footer-container">
+
+    <!-- Column 1: Address & Contact -->
+    <div class="footer-col">
+      <p>
+        G-9/ 85 RATIYA MARG SANGAM VIHAR <br>
+        NEW DELHI
+      </p>
+
+      <p class="footer-email">
+        Email:
+        <a href="mailto:info@rrcomputers.net">info@rrcomputers.net</a>
+      </p>
+
+      <div class="footer-social">
+        <a href="#" aria-label="Facebook" target="_blank"> <img
+            src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></a>
+        <a href="https://www.instagram.com/" aria-label="Instagram" target="_blank"><img
+            src="<?php echo get_template_directory_uri(); ?>/img/instagram.png" alt=""></a>
+        <a href="#" aria-label="Twitter" target="_blank"><img
+            src="<?php echo get_template_directory_uri(); ?>/img/1707222563twitter-logo-png.png" alt=""></a>
+
+      </div>
     </div>
-  </footer>
-    <!-- script 1 -->
-    <script>
-    function toggleMenu() {
-      document.getElementById("navMenu").classList.toggle("show");
-    }
-  </script>
+
+    <!-- Column 2: Pages -->
+    <div class="footer-col">
+      <h4>Qucik Links</h4>
+      <ul>
+        <li><a href="<?php echo site_url('/'); ?>">Home</a></li>
+        <li><a href="<?php echo site_url('/about'); ?>">About</a></li>
+        <li><a href="<?php echo site_url('/services'); ?>">Services</a></li>
+        <li><a href="<?php echo site_url('/contact'); ?>">Contact</a></li>
+        <li><a href="<?php echo site_url('/rental'); ?>">Rental</a></li>
+      </ul>
+    </div>
+
+    <!-- Column 3: Services -->
+    <div class="footer-col">
+      <h4>Services</h4>
+      <ul>
+        <li><a href="<?php echo site_url('/services'); ?>">Laptop Repair / Computer Repair</a></li>
+        <li><a href="<?php echo site_url('/services'); ?>">Cleaning Laptops / Replacing Thermal Paste</a></li>
+        <li><a href="<?php echo site_url('/services'); ?>">Data Recovery</a></li>
+        <li><a href="<?php echo site_url('/services'); ?>">OS Installation</a></li>
+        <li><a href="<?php echo site_url('/services'); ?>">Assembling a Personal Computer at Home</a></li>
+      </ul>
+    </div>
+
+  </div>
+
+  <!-- Bottom -->
+  <div class="footer-bottom">
+    © 2026 RR COMPUTERS.
+  </div>
+</footer>
+<!-- script 1 -->
+<script>
+  function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("show");
+  }
+</script>
 <?php get_footer(); ?>
